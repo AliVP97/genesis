d2974be455c506dbb65ba4a851aacf9253acc368
Ali Valipour <ali.valipour@hotmail.com>
2025-07-23 14:29:23 +0330

    Remove hardcoded styles from Button component for improved flexibility

--- libs/ui/src/lib/button.tsx
+++ libs/ui/src/lib/button.tsx
@@ -3,5 +3,5 @@ import React from 'react';
 type ButtonProps = React.ButtonHTMLAttributes<HTMLButtonElement>;
 
 export const Button: React.FC<ButtonProps> = (props) => {
-  return <button className="bg-black rounded-md p-4" {...props} />;
+  return <button {...props} />;
 };
